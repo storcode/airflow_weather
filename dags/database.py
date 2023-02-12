64d1376d6beb16b5c86052135e76920154f00a27
Edward <4ubais04@gmail.com>
2023-02-12 13:21:52 +0300

    New_project

---
+++ dags/database.py
@@ -0,0 +1,110 @@
+import json
+
+
+def insert_weather(cursor, date_downloads, time_downloads, r):
+    cursor.execute(
+        "INSERT INTO public.weather (date_downloads,time_downloads,coord,weather,base,main,visibility,wind,clouds,dt,sys,timezone,name)"
+        "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        (date_downloads, time_downloads,
+         json.dumps(r["coord"]), json.dumps(r["weather"]), r["base"], json.dumps(r["main"]), r["visibility"],
+         json.dumps(r["wind"]), json.dumps(r["clouds"]), r["dt"], json.dumps(r["sys"]), r["timezone"], r["name"]))
+    count_weather = cursor.rowcount
+    return count_weather
+
+def insert_dim_coordinates(cursor):
+    cursor.execute("""
+    insert into dwh.dim_coordinates (longitude, latitude)
+	    select (select * from json_to_record(w.coord) as x(lon float)) as longitude,
+	    (select * from json_to_record(w.coord) as x(lat float)) as latitude
+	    from public.weather  w
+        where w.id > (select max(coord_id) from dwh.dim_coordinates)"""
+    count_dim_coordinates = cursor.rowcount
+    return count_dim_coordinates
+
+
+def insert_dim_date(cursor):
+    cursor.execute("""
+    insert into dwh.dim_date (full_date, initial_date, "year", "month", "day", "quarter", number_week , day_week, day_year, month_year)
+        select  (select to_char(w.date_downloads, 'YYYYMMDD')::int4) as full_date,
+                w.date_downloads,
+                (select extract(year from w.date_downloads)) as "year",
+                (select extract(month from w.date_downloads)) as "month",
+                (select extract(day from w.date_downloads)) as "day",
+                (select extract(quarter from w.date_downloads)) as "quarter",
+                (select extract(week from w.date_downloads)) as number_week,
+                (select extract(isodow from w.date_downloads)) as day_week,
+                (select extract(doy from w.date_downloads)) as day_year,
+                (select extract(month from w.date_downloads)) as month_year
+        from public.weather w
+        where w.id > (select max(date_id) from dwh.dim_date)"""
+    count_dim_date = cursor.rowcount
+    return count_dim_date
+
+
+def insert_dim_sun_light(cursor):
+    cursor.execute("""
+    insert into dwh.dim_sun_light (sunrise, sunset, sunrise_unix, sunset_unix)
+        select (select * from json_to_record(w.sys) as x(sunrise int4)) as sunrise,
+        (select * from json_to_record (w.sys) as x(sunset int4)) as sunset,
+        (select timestamp with time zone 'epoch' + (select * from json_to_record (w.sys) as x(sunrise int4)) * interval '1 second') as sunrise_unix,
+        (select timestamp with time zone 'epoch' + (select * from json_to_record (w.sys) as x(sunset int4)) * interval '1 second') as sunset_unix
+        from public.weather w
+        where w.id > (select max(sun_light_id) from dwh.dim_sun_light)"""
+    count_dim_sun_light = cursor.rowcount
+    return count_dim_sun_light
+
+
+def insert_dim_time(cursor):
+    cursor.execute("""
+    insert into dwh.dim_time (full_time, initial_time, "hour", "minute", "second", night , morning, afternoon, evening)
+        select  (select to_char(w.time_downloads, 'HH24MISS')::int4) as full_time,
+                w.time_downloads,
+                (select extract(hour from w.time_downloads)) as "hour",
+                (select extract(minute from w.time_downloads)) as "minute",
+                (select extract(second from w.time_downloads)) as "second",
+                (select case when w.time_downloads between '00:00:00' and '05:59:59' then 'night' end) as night,
+                (select case when w.time_downloads between '06:00:00' and '11:59:59' then 'morning' end) as morning,
+                (select case when w.time_downloads between '12:00:00' and '17:59:59' then 'afternoon' end) as afternoon,
+                (select case when w.time_downloads between '18:00:00' and '23:59:59' then 'evening' end) as evening
+        from public.weather w
+        where w.id > (select max(time_id) from dwh.dim_time)"""
+    count_dim_time = cursor.rowcount
+    return count_dim_time
+
+
+def insert_dim_timezone(cursor):
+    cursor.execute("""
+    insert into dwh.dim_timezone (timezone)
+        select w.timezone
+        from public.weather w
+        where w.id > (select max(timezone_id) from dwh.dim_timezone)"""
+    count_dim_timezone = cursor.rowcount
+    return count_dim_timezone
+
+
+def insert_dim_timezone_name(cursor):
+    cursor.execute("""
+    insert into dwh.dim_timezone_name ("name", country)
+        select w."name",
+        (select * from json_to_record(w.sys) as x(country text)) as country
+        from public.weather w
+        where w.id > (select max(timezone_name_id) from dwh.dim_timezone_name)"""
+    count_dim_timezone_name = cursor.rowcount
+    return count_dim_timezone_name
+
+
+def insert_dim_weather_descr(cursor):
+    cursor.execute("""
+    insert into dwh.dim_weather_descr (condition_id, group_main_params, weather_condition_groups, clouds)
+        select (select * from json_to_recordset(w.weather) as x(id int4)) as condition_id,
+        (select * from json_to_recordset(w.weather) as x(main text)) as group_main_params,
+        (select * from json_to_recordset(w.weather) as x(description text)) as weather_condition_groups,
+        (select * from json_to_record(w.clouds) as x("all" int4)) as clouds
+        from public.weather w
+        where w.id > (select max(weather_descr_id) from dwh.dim_weather_descr)"""
+    count_dim_weather_descr = cursor.rowcount
+    return count_dim_weather_descr
+
+
+    insert into dwh.fact_weather (dim_coordinates_id, dim_date_id, dim_sun_light_id, dim_time_id, dim_timezone_id, dim_timezone_name_id, dim_weather_descr_id, temperature, feels_like, temp_min, temp_max, pressure, humidity, visibility)
+        select ******
---
+++ dags/init.sql
@@ -0,0 +1,128 @@
+CREATE SCHEMA IF NOT EXISTS dwh2;
+
+CREATE TABLE IF NOT EXISTS dwh2.weather (
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	date_downloads date NULL,
+	time_downloads time NULL,
+	coord json NULL,
+	weather json NULL,
+	base text NULL,
+	main json NULL,
+	visibility text NULL,
+	wind json NULL,
+	clouds json NULL,
+	dt int4 NULL,
+	sys json NULL,
+	timezone int4 NULL,
+	name text NULL,
+	CONSTRAINT pk_weather PRIMARY KEY (id)
+);
+
+create table dwh2.dim_coordinates
+(
+	coord_id int4 generated always as identity,
+	longitude float not null,
+	latitude float not null,
+	sys_ts timestamp(0) default now(),
+	constraint pk_dim_coordinates primary key(coord_id)
+);
+
+create table dwh2.dim_date 
+(
+	date_id int4 generated always as identity,
+	full_date int4 not null,
+	initial_date date not null,
+	"year" int4 not null,
+	"month" int4 not null,
+	"day" int4 not null,
+	"quarter" int4 not null,
+	number_week int4 not null,
+	day_week int4 not null,
+	day_year int4 not null,
+	month_year int4 not null,
+	sys_ts timestamp(0) default now(),
+	constraint pk_date primary key (date_id)
+);
+
+create table dwh2.dim_sun_light
+(
+	sun_light_id int4 generated always as identity,
+	sunrise int4 not null,
+	sunset int4 not null,
+	sunrise_unix timestamp not null,
+	sunset_unix timestamp not null,
+	sys_ts timestamp(0) default now(),
+	constraint pk_dim_sun_light primary key(sun_light_id)
+);
+
+create table dwh2.dim_time
+(
+	time_id int4 generated always as identity,
+	full_time int4 not null,
+	initial_time time not null,
+	"hour" int4 not null,
+	"minute" int4 not null,
+	"second" int4 not null,
+	night text,
+	morning text,
+	afternoon text,
+	evening text,
+	sys_ts timestamp(0) default now(),
+	constraint pk_dim_time primary key (time_id)
+);
+
+create table dwh2.dim_timezone
+(
+	timezone_id int4 generated always as identity,
+	timezone int4 not null,
+	sys_ts timestamp(0) default now(),
+	constraint pk_dim_timezone primary key(timezone_id)
+);
+
+create table dwh2.dim_timezone_name
+(
+	timezone_name_id int4 generated always as identity,
+	"name" text not null,
+	country text not null,
+	sys_ts timestamp(0) default now(),
+	constraint pk_dim_timezone_name primary key(timezone_name_id)
+);
+
+create table dwh2.dim_weather_descr
+(
+	weather_descr_id int4 generated always as identity,
+	condition_id int4 not null,
+	group_main_params text not null,
+	weather_condition_groups text not null,
+	clouds int4 not null,
+	sys_ts timestamp(0) default now(),
+	constraint pk_dim_weather_descr primary key (weather_descr_id)
+);
+
+create table dwh2.fact_weather
+(
+	fact_weather_id int4 generated always as identity,
+	dim_coordinates_id int4 not null,
+	dim_date_id int4 not null,
+	dim_sun_light_id int4 not null,
+	dim_time_id int4 not null,
+	dim_timezone_id int4 not null,
+	dim_timezone_name_id int4 not null,
+	dim_weather_descr_id int4 not null,
+	temperature float not null,
+	feels_like float not null,
+	temp_min float not null,
+	temp_max float not null,
+	pressure int4 not null,
+	humidity int4 not null,
+	visibility int4 not null,
+	sys_ts timestamp(0) default now(),
+	CONSTRAINT pk_fact_weather PRIMARY KEY (fact_weather_id),
+	CONSTRAINT fk_coord FOREIGN KEY (dim_coordinates_id) REFERENCES dwh2.dim_coordinates(coord_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_dim_date FOREIGN KEY (dim_date_id) REFERENCES dwh2.dim_date(date_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_sun_light FOREIGN KEY (dim_sun_light_id) REFERENCES dwh2.dim_sun_light(sun_light_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_time FOREIGN KEY (dim_time_id) REFERENCES dwh2.dim_time(time_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_timezone FOREIGN KEY (dim_timezone_id) REFERENCES dwh2.dim_timezone(timezone_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_timezone_name FOREIGN KEY (dim_timezone_name_id) REFERENCES dwh2.dim_timezone_name(timezone_name_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_weather_descr FOREIGN KEY (dim_weather_descr_id) REFERENCES dwh2.dim_weather_descr(weather_descr_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
