5dda827a0f16e810664b9130ee3bea5209ed4693
Edward <4ubais04@gmail.com>
2023-02-22 17:36:33 +0300

    улучшайзинг) - пока ничего существенного

--- dags/database.py
+++ dags/database.py
@@ -1,13 +1,14 @@
 import json
 
 
-def insert_weather(cursor, date_downloads, time_downloads, r):
+def insert_weather(cursor, date_downloads, time_downloads, reg_json):
     cursor.execute(
-        """INSERT INTO dwh.weather (date_downloads,time_downloads,coord,weather,base,main,visibility,wind,clouds,dt,sys,timezone,name)"""
-        """VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+        """INSERT INTO dwh.weather (date_downloads,time_downloads,coord,weather,base,main,visibility,wind,clouds,
+        dt,sys,timezone,name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
         (date_downloads, time_downloads,
-         json.dumps(r["coord"]), json.dumps(r["weather"]), r["base"], json.dumps(r["main"]), r["visibility"],
-         json.dumps(r["wind"]), json.dumps(r["clouds"]), r["dt"], json.dumps(r["sys"]), r["timezone"], r["name"]))
+         json.dumps(reg_json["coord"]), json.dumps(reg_json["weather"]), reg_json["base"], json.dumps(reg_json["main"]),
+         reg_json["visibility"], json.dumps(reg_json["wind"]), json.dumps(reg_json["clouds"]), reg_json["dt"],
+         json.dumps(reg_json["sys"]), reg_json["timezone"], reg_json["name"]))
     count_weather = cursor.rowcount
     return count_weather
 
@@ -16,7 +17,7 @@ def insert_dim_coordinates(cursor):
     cursor.execute("""
     insert into dwh.dim_coordinates (longitude, latitude)
 	    select (select * from json_to_record(w.coord) as x(lon float)) as longitude,
-	    (select * from json_to_record(w.coord) as x(lat float)) as latitude
+	        (select * from json_to_record(w.coord) as x(lat float)) as latitude
 	    from public.weather  w
         where w.id > (select max(coord_id) from dwh.dim_coordinates""")
     count_dim_coordinates = cursor.rowcount
@@ -25,7 +26,8 @@ def insert_dim_coordinates(cursor):
 
 def insert_dim_date(cursor):
     cursor.execute("""
-    insert into dwh.dim_date (full_date, initial_date, "year", "month", "day", "quarter", number_week , day_week, day_year, month_year)
+    insert into dwh.dim_date (full_date, initial_date, "year", "month", "day", "quarter", number_week,
+    day_week, day_year, month_year)
         select  (select to_char(w.date_downloads, 'YYYYMMDD')::int4) as full_date,
                 w.date_downloads,
                 (select extract(year from w.date_downloads)) as "year",
@@ -47,8 +49,10 @@ def insert_dim_sun_light(cursor):
     insert into dwh.dim_sun_light (sunrise, sunset, sunrise_unix, sunset_unix)
         select (select * from json_to_record(w.sys) as x(sunrise int4)) as sunrise,
         (select * from json_to_record (w.sys) as x(sunset int4)) as sunset,
-        (select timestamp with time zone 'epoch' + (select * from json_to_record (w.sys) as x(sunrise int4)) * interval '1 second') as sunrise_unix,
-        (select timestamp with time zone 'epoch' + (select * from json_to_record (w.sys) as x(sunset int4)) * interval '1 second') as sunset_unix
+        (select timestamp with time zone 'epoch' + (select * from json_to_record (w.sys)
+            as x(sunrise int4)) * interval '1 second') as sunrise_unix,
+        (select timestamp with time zone 'epoch' + (select * from json_to_record (w.sys)
+            as x(sunset int4)) * interval '1 second') as sunset_unix
         from public.weather w
         where w.id > (select max(sun_light_id) from dwh.dim_sun_light""")
     count_dim_sun_light = cursor.rowcount
@@ -108,5 +112,7 @@ def insert_dim_weather_descr(cursor):
 
 # def insert_fact_weather(cursor):
 #    cursor.execute("""
-#    insert into fact_weather (dim_coordinates_id, dim_date_id, dim_sun_light_id, dim_time_id, dim_timezone_id, dim_timezone_name_id, dim_weather_descr_id, temperature, feels_like, temp_min, temp_max, pressure, humidity, visibility)
+#    insert into fact_weather (dim_coordinates_id, dim_date_id, dim_sun_light_id, dim_time_id,
+#    dim_timezone_id, dim_timezone_name_id, dim_weather_descr_id, temperature, feels_like,
+#    temp_min, temp_max, pressure, humidity, visibility)
 #        select ******""")
--- dags/weather_data.py
+++ dags/weather_data.py
@@ -1,7 +1,6 @@
 import psycopg2
 from psycopg2 import OperationalError
 import requests
-# import json
 import pytz
 import datetime as dt
 from airflow import DAG
@@ -13,43 +12,28 @@ from database import *
 def download():
     import key_appid
     url = f'https://api.openweathermap.org/data/2.5/weather?q=Cheboksary,ru&APPID={key_appid.key_appid}&units=metric'
-    r = requests.get(url=url).json()
+    reg_json = requests.get(url=url).json()
 
     with open('weather_city.json', 'w') as json_file:
-        json.dump(r, json_file)
+        json.dump(reg_json, json_file)
     print("Файл успешно скачан")
-    return r
+    return reg_json
 
 
-def create_connection_db():
-    import key_PSQL
-    connection = None
-    try:
-        connection = psycopg2.connect(user="postgres",
-                                      password="postgres",
-                                      host="postgres",  # название контейнера в docker-compose
-                                      port="5432",
-                                      database="postgres")
-        print("Подключение к базе PostgreSQL успешно")
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-    return connection
-
-
-def process_weather_data(r):
+def process_weather_data(reg_json):
     import key_PSQL
     msc = pytz.timezone('europe/moscow')
     date_downloads = datetime.now(msc).strftime("%Y-%m-%d")
     time_downloads = datetime.now(msc).strftime("%H:%M:%S")
     try:
-        connection = psycopg2.connect(user="postgres",
-                                      password="postgres",
+        connection = psycopg2.connect(user=key_PSQL.user,
+                                      password=key_PSQL.password,
                                       host="postgres",  # название контейнера в docker-compose
                                       port="5432",
                                       database="postgres")
         print("Подключение к базе PostgreSQL выполнено")
         cursor = connection.cursor()
-        count_weather = insert_weather(cursor, date_downloads, time_downloads, r)
+        count_weather = insert_weather(cursor, date_downloads, time_downloads, reg_json)
         print(count_weather, "Запись успешно вставлена в таблицу 'weather'")
         count_dim_coordinates = insert_dim_coordinates(cursor)
         print(count_dim_coordinates, "Запись успешно вставлена в таблицу 'dim_coordinates'")
@@ -65,8 +49,8 @@ def process_weather_data(r):
         print(count_dim_timezone_name, "Запись успешно вставлена в таблицу 'dim_timezone_name'")
         count_dim_weather_descr = insert_dim_weather_descr(cursor)
         print(count_dim_weather_descr, "Запись успешно вставлена в таблицу 'dim_weather_descr'")
- #       count_fact_weather = insert_fact_weather(cursor)
- #       print(count_fact_weather, "Запись успешно вставлена в таблицу 'fact_weather'")
+        #      count_fact_weather = insert_fact_weather(cursor)
+        #      print(count_fact_weather, "Запись успешно вставлена в таблицу 'fact_weather'")
         connection.commit()
         cursor.close()
         connection.close()
@@ -81,7 +65,7 @@ args = {
     'retries': 1,
     'retry_delay': dt.timedelta(minutes=1),
     'schedule_interval': '*/5 * * * *',
-    'depends_on_past': False,
+    'depends_on_past': False
 }
 
 with DAG(dag_id='weather', default_args=args) as dag:
@@ -90,15 +74,10 @@ with DAG(dag_id='weather', default_args=args) as dag:
         python_callable=download,
         dag=dag
     )
-    connection_db = PythonOperator(
-        task_id='create_connection_db',
-        python_callable=create_connection_db,
-        dag=dag
-    )
     weather_data = PythonOperator(
-        task_id='proces_weather_data',
+        task_id='process_weather_data',
         python_callable=process_weather_data,
+        # op_kwargs=download(),
         dag=dag
     )
-    file_download >> connection_db >> weather_data
-
+    file_download >> weather_data
